94cf6562e92f82ceef648950b22b54a7ef64c981
nange-ustb@163.com <sa@ubuntu.(none)>
2014-02-14 04:32:22 +0300

    checkout a branch

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 RailsWebPrototype::Application.config.secret_token = '54948f3b78a4bb4b9906ab5d1e8ec37b5332e401355e73a2acc4f3801e68a501384dbe181138753314068f242dd59339f3f9a876eb57839df96dccc9893d73e0'
