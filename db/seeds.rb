32b61a8ca8c2bdc50977a0be51c477335ca4d118
nange-ustb@163.com <sa@ubuntu.(none)>
2014-02-14 04:22:22 +0300

    delete something

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,4 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
 
-Administrator.find_or_create_by_user_name(:user_name => 'qiwl', :real_name => '齐文龙',:is_admin=>true)
-Administrator.find_or_create_by_user_name(:user_name => 'wanggc', :real_name => '王国超',:is_admin=>true)
-Administrator.find_or_create_by_user_name(:user_name => 'duhw', :real_name => '杜宏伟',:is_admin=>true)
+ 
